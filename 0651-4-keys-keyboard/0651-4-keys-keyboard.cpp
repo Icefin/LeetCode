66b91bfa22a10c022c70ec096df66b9afc795650
GeonHo Shin <76864202+Icefin@users.noreply.github.com>
2023-03-31 13:04:11 +0900

    Time: 3 ms (45.18%), Space: 6 MB (92.17%) - LeetHub

--- 0651-4-keys-keyboard/0651-4-keys-keyboard.cpp
+++ 0651-4-keys-keyboard/0651-4-keys-keyboard.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int maxA(int n) {
-        vector<int> dp(n + 1, 0);
-        
         if (n <= 6)
             return (n);
-        for (int i = 1; i <= 6; i++)
-            dp[i] = i;
         
+        int res = 0;
+        int prev = 6, pprev = 5, doub = 4, trip = 3, quad = 2;
         for(int i = 7; i <= n; i++)
         {
-            int tmp = -1;
-            tmp = std::max(tmp, 1 + dp[i - 1]);
-            tmp = std::max(tmp, 2 * dp[i - 3]);
-            tmp = std::max(tmp, 3 * dp[i - 4]);
-            tmp = std::max(tmp, 4 * dp[i - 5]);
-            dp[i] = tmp;
+            res = max(2 * doub, max(3 * trip, 4 * quad));
+            
+            quad = trip;
+            trip = doub;
+            doub = pprev;
+            pprev = prev;
+            prev = res;
         }
 
-        return dp[n];
+        return (res);
     }
 };
