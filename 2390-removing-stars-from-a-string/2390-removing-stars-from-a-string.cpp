a81d54178caf29bbd1831122f9ce6eab2e6bd672
GeonHo Shin <76864202+Icefin@users.noreply.github.com>
2023-04-11 09:07:43 +0900

    Time: 80 ms (80.39%), Space: 25.7 MB (81.24%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
-    string removeStars(string s) {
-        deque<char> dq;
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == '*') {
-                dq.pop_back();
-                continue;
-            }
-            dq.push_back(s[i]);
-        }
-        string res = "";
-        while (!dq.empty()) {
-            res += dq.front();
-            dq.pop_front();
-        }
+     string removeStars(string s) {
+        string res;
+        for (char c : s)
+            if (c == '*')
+                res.pop_back();
+            else
+                res += c;
         return res;
     }
 };
