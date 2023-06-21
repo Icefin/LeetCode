813ea210fd4645f13cd123473460bd343caa2fde
GeonHo Shin <76864202+Icefin@users.noreply.github.com>
2023-06-21 17:14:31 +0900

    Time: 111 ms (55.84%), Space: 38.5 MB (71.17%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     long long minCost(vector<int>& nums, vector<int>& cost) {
+        long long res;
         int n = nums.size();
-        vector<pair<int, int>> v;
-        for (int i = 0; i < n; i++)
-            v.push_back({nums[i], cost[i]});
-        sort(v.begin(), v.end());
         
-        long long res = 0;
-        int left = v[0].first;
-        int right = v[n - 1].first;
+        int left = *min_element(nums.begin(), nums.end());
+        int right = *max_element(nums.begin(), nums.end());
         
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            long long mcost = getCost(v, mid);
-            long long mpcost = getCost(v, mid + 1);
+            long long mcost = getCost(nums, cost, mid);
+            long long mpcost = getCost(nums, cost, mid + 1);
             res = min(mcost, mpcost);
             if (mcost > mpcost)
                 left = mid + 1;
@@ -25,11 +21,11 @@ public:
     }
     
 private :
-    long long getCost(vector<pair<int, int>>& v, int target) {
+    long long getCost(vector<int>& nums, vector<int>& cost, int target) {
         long long res = 0;
-        int n = v.size();
+        int n = nums.size();
         for (int i = 0; i < n; i++)
-            res += (long long)v[i].second * abs(v[i].first - target);
+            res += (long long)cost[i] * abs(nums[i] - target);
         return res;
     }
 };
