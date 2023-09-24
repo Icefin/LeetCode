6e9f0f093fe7fe26e1e13fb7f467e4a7a45d0819
GeonHo Shin <76864202+Icefin@users.noreply.github.com>
2023-09-24 16:33:57 +0900

    Time: 6 ms (66.91%), Space: 6.4 MB (76.54%) - LeetHub

---
+++ 0799-champagne-tower/0799-champagne-tower.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double champagneTower(int poured, int qi, int qj) {
+        tower[0][0] = poured;
+        for (int i = 0; i <= 100; ++i)
+        {
+            for (int j = 0; j <= i; ++j)
+            {
+                if (tower[i][j] > 1.0)
+                {
+                    if (i <= 99 && j <= i)
+                    {
+                        tower[i + 1][j] += (tower[i][j] - 1.0) / 2.0;
+                        tower[i + 1][j + 1] += (tower[i][j] - 1.0) / 2.0;    
+                    }
+                    tower[i][j] = 1.0;
+                }
+            }
+        }
+        return tower[qi][qj];
+    }
+    
+private :
+    double tower[101][101];
+};
