95a3b00c9e7aa53442e4e582808c4450a478c174
GeonHo Shin <76864202+Icefin@users.noreply.github.com>
2023-09-25 11:36:08 +0900

    Time: 0 ms (100.00%), Space: 7.2 MB (8.68%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        int s_alpha[27];
+        int t_alpha[27];
         
+        memset(s_alpha, 0, sizeof(s_alpha));
+        memset(t_alpha, 0, sizeof(t_alpha));
         int n = s.length();
-        for (int i = 0; i < n; ++i)
-            if (s[i] != t[i])
-                return t[i];
-        return t[n];
+        for (int i = 0; i < n; ++i) {
+            s_alpha[s[i] - 'a']++;
+            t_alpha[t[i] - 'a']++;
+        }
+        t_alpha[t[n] - 'a']++;
+        for (int i = 0; i < 27; ++i)
+            if (s_alpha[i] != t_alpha[i])
+                return ('a' + i);
+        return 0;
     }
 };
