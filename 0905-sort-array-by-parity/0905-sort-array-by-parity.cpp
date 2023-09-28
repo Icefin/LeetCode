6fca5d401b155efe9a1a561f7a73d2ecf2128c79
GeonHo Shin <76864202+Icefin@users.noreply.github.com>
2023-09-28 13:32:24 +0900

    Time: 13 ms (17.42%), Space: 17.6 MB (6.46%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        int pos = 0;
+        vector<pair<int, int>> remain_pair;
         
         int n = nums.size();
-        for (int i = 0; i < n; ++i) {
-            if (nums[i] % 2 == 0) {
-                int temp = nums[pos];
-                nums[pos] = nums[i];
-                nums[i] = temp;
-                pos++;
-            }
-        }
+        for (int i = 0; i < n; ++i)
+            remain_pair.push_back({nums[i] % 2, nums[i]});
+        
+        sort(remain_pair.begin(), remain_pair.end());
+        for (int i = 0; i < n; ++i)
+            nums[i] = remain_pair[i].second;
+        
         return nums;
     }
 };
