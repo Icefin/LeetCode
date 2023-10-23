f26bf3c0eb5a7acdfb78f73f3ade9c859325ae07
GeonHo Shin <wjrchorh135@gmail.com>
2023-10-23 13:27:11 +0900

    Time: 0 ms (100.00%), Space: 6.3 MB (26.74%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -3,11 +3,6 @@ public:
     bool isPowerOfFour(int n) {
         if (n < 1)
             return false;
-        while (n > 1) {
-            if (n % 4 != 0)
-                return false;
-            n /= 4;
-        }
-        return true;
+        return (n && !(n & (n - 1)) && !(n & 0xAAAAAAAA));
     }
 };
