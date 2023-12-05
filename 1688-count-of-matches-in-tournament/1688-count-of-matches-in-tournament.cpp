78acca19d5f90f8a44c60c3396fe5951b46038d9
GeonHo Shin <wjrchorh135@gmail.com>
2023-12-05 13:38:54 +0900

    Time: 0 ms (100.00%), Space: 6.4 MB (36.15%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     int numberOfMatches(int n) {
-        int res = 0;
-        while (n > 1) {
-            res += n / 2;
-            if (n % 2 == 0)
-                n = n / 2;
-            else 
-                n = n / 2 + 1;
-        }
-        return res;
+        return  (n - 1);
     }
 };
