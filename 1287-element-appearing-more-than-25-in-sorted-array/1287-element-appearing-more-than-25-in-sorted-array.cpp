b516f3d66583410f5eb844d945f5a273df0cc109
GeonHo Shin <wjrchorh135@gmail.com>
2023-12-11 14:17:50 +0900

    Time: 8 ms (78.76%), Space: 12.5 MB (99.70%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
@@ -2,13 +2,17 @@ class Solution {
 public:
     int findSpecialInteger(vector<int>& arr) {
         int n = arr.size();
-        int l = 0;
+        vector<int> candidates = { arr[n / 4], arr[n / 2], arr[3 * n / 4] };
+        int target = n / 4;
         
-        for (int r = n / 4; r < n; ++r) {
-            if (arr[l] == arr[r])
-                return arr[l];
-            ++l;
+        for (int candidate : candidates) {
+            int left = lower_bound(arr.begin(), arr.end(), candidate) - arr.begin();
+            int right = upper_bound(arr.begin(), arr.end(), candidate) - arr.begin() - 1;
+        
+            if (right - left + 1 > target)
+                return candidate;
         }
-        return 0;
+        
+        return -1;
     }
 };
