156131ba641be7404a0f8838ddf18a2683bc42a9
GeonHo Shin <wjrchorh135@gmail.com>
2023-12-20 12:47:32 +0900

    Time: 19 ms (19.09%), Space: 46.9 MB (58.30%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        sort(prices.begin(), prices.end());
+        
+        int price = prices[0] + prices[1];
+        if (price > money)
+            return money;
+        return money - price;
+    }
+};
