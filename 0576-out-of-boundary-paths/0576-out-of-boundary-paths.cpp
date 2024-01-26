d6e0b3d0475be42fdb52fe3853579192b75e0a51
GeonHo Shin <wjrchorh135@gmail.com>
2024-01-26 20:33:51 +0900

    Time: 10 ms (42.77%), Space: 10.4 MB (23.69%) - LeetHub

---
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(maxMove + 1, -1)));
+        int res = dfs(startRow, startColumn, 0, m, n, maxMove, dp);
+        return res;
+    }
+    
+private :
+    const int dr[4] = {-1, 0, 1, 0};
+    const int dc[4] = {0, 1, 0, -1};
+    
+    int dfs(int r, int c, int moveCnt, int m, int n, int maxMove, vector<vector<vector<int>>>& dp) {
+        if (moveCnt > maxMove)
+            return 0;
+        if (isOutofBoundary(r, c, m, n) == true)
+            return 1;
+        
+        if (dp[r][c][moveCnt] != -1)
+            return dp[r][c][moveCnt];
+        
+        int paths = 0;
+        for (int i = 0; i < 4; ++i) {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+            paths = (paths + dfs(nr, nc, moveCnt + 1, m, n, maxMove, dp)) % 1000000007;
+        }
+        dp[r][c][moveCnt] = paths;
+        return paths;
+    }
+    
+    bool isOutofBoundary(int r, int c, int m, int n) {
+        if (r < 0 || c < 0 || r >= m || c >= n)
+            return true;
+        return false;
+    }
+};
