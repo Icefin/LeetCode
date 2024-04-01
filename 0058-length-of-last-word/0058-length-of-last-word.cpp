b5536bd76f8caea523f8301c4fa48e9b833692be
GeonHo Shin <wjrchorh135@gmail.com>
2024-04-01 21:50:27 +0900

    Time: 4 ms (47.87%), Space: 7.8 MB (20.26%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int ans = 0;
+        int res = 0;
         for (int i = 0; i < s.length(); i++) {
             if (s[i] != ' ') {
-                ans = 0;
+                res = 0;
                 while (i < s.length() && s[i] != ' ') {
-                    ans++;
+                    res++;
                     i++;
                 }
             }
         }
-        return ans;
+        return res;
     }
 };
